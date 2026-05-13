ad2eb80c4a3282f5c9c69215c7140ad8b54000fe
Evgeny2015 <105362989+Evgeny2015@users.noreply.github.com>
2026-05-13 18:34:02 +0300

    DZ8 is completed

--- Services/SimpleStore/UserProfile.cs
+++ Services/SimpleStore/UserProfile.cs
@@ -16,5 +16,19 @@ namespace SimpleStore
                 writer.Write(CreatedAt.Ticks);
             }
         }
+
+        public static UserProfile DeserializeFromBinary(System.IO.Stream stream)
+        {
+            using (var reader = new System.IO.BinaryReader(stream, System.Text.Encoding.UTF8, leaveOpen: true))
+            {
+                var profile = new UserProfile
+                {
+                    Id = reader.ReadInt32(),
+                    Username = reader.ReadString(),
+                    CreatedAt = new DateTime(reader.ReadInt64())
+                };
+                return profile;
+            }
+        }
     }
 }
